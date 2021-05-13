9808525d04bb5721165dbbc0825a8ed756762d2b
xIRoXaSx <xIRoXaSx@LizardNetwork.net>
2021-05-13 02:15:00 +0200

    Add project files.

---
+++ TwitchNotifier/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using TwitchNotifier.src.config;
+
+namespace TwitchNotifier {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+            Parser.Serialize(new Config());
+        }
+    }
+}
