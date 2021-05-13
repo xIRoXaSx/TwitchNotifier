16acb3a52f553f0a7a7fd60da07f1f8d2c298fc5
RoXaS <log2.me@outlook.com>
2021-05-13 02:15:00 +0200

    Add project files.

---
+++ TwitchNotifier/src/config/Parser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
+namespace TwitchNotifier.src.config {
+    class Parser {
+
+        /// <summary>
+        /// Serialize an object to a string
+        /// </summary>
+        /// <param name="objectToSerialze">The object to serialze</param>
+        public static void Serialize(object objectToSerialze) {
+            var serializer = new SerializerBuilder()
+                //.WithNamingConvention(CamelCaseNamingConvention.Instance)
+                .Build();
+
+            var yaml = serializer.Serialize(objectToSerialze);
+            Console.WriteLine(yaml);
+        }
+    }
+}
