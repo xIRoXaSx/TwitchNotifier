5b9f8b33b0167c0539943b8b635818d61ca278ab
xIRoXaSx <xiroxasx@lizardnetwork.net>
2021-05-26 01:09:16 +0200

    Added additional properties for the embed

--- TwitchNotifier/src/Twitch/LiveMonitoring.cs
+++ TwitchNotifier/src/Twitch/LiveMonitoring.cs
@@ -9,8 +9,9 @@ using TwitchLib.Api.Services;
 using TwitchLib.Api.Services.Events;
 using TwitchLib.Api.Services.Events.LiveStreamMonitor;
 using TwitchNotifier.src.config;
+using TwitchNotifier.src.Helper;
 using TwitchNotifier.src.Logging;
-using TwitchNotifier.src.WebRequest;
+using TwitchNotifier.src.WebRequests;
 using YamlDotNet.Serialization;
 
 namespace TwitchNotifier.src.Twitch {
@@ -43,26 +44,24 @@ namespace TwitchNotifier.src.Twitch {
                 var usernames = new List<string>();
                 foreach (var property in typeof(TwitchNotifierSettings).GetProperties()) {
                     // Events like "OnStreamStart", "OnStreamEnd", "OnFollow", ...
-                    //var twitchEvent = property.GetValue(config["TwitchNotifier"]);
                     var twitchEvent = config["TwitchNotifier"][property.Name];
 
-                    // eventNode is the key node for all settings below each event
-                    foreach (var eventNode in typeof(Event).GetProperties()) {
-                        //usernames.AddRange(((EventObject)eventNode.GetValue(twitchEvent)).Twitch.Usernames);
-                        List<string> usernameList = ((List<object>)twitchEvent[eventNode.Name]["Twitch"]["Usernames"]).Select(x => (string)x).ToList();
+                    // eventNode is the node for all settings below each event
+                    foreach (var eventNode in twitchEvent) {
+                        List<string> usernameList = ((List<object>)eventNode.Value["Twitch"]["Usernames"]).Select(x => (string)x).ToList();
                         usernames.AddRange(usernameList);
                     }
                 }
 
+                //Monitor.OnStreamUpdate += Monitor_OnStreamUpdate;
+                Monitor.OnStreamOnline += Monitor_OnStreamOnline;
+                Monitor.OnStreamOffline += Monitor_OnStreamOffline;
+                Monitor.OnServiceStarted += Monitor_OnServiceStarted;
+                Monitor.OnChannelsSet += Monitor_OnChannelsSet;
+
                 Monitor.SetChannelsByName(usernames.Distinct().ToList());
 
                 await Monitor.UpdateLiveStreamersAsync();
-
-                Monitor.OnStreamOnline += Monitor_OnStreamOnline;
-                Monitor.OnStreamOffline += Monitor_OnStreamOffline;
-                //Monitor.OnStreamUpdate += Monitor_OnStreamUpdate;
-                Monitor.OnServiceStarted += Monitor_OnServiceStarted;
-                Monitor.OnChannelsSet += Monitor_OnChannelsSet;
 
                 Monitor.Start();
                 await API.Helix.Channels.CheckCredentialsAsync();
@@ -75,41 +74,62 @@ namespace TwitchNotifier.src.Twitch {
         }
 
         private void Monitor_OnChannelsSet(object sender, OnChannelsSetArgs e) {
-            Logging.Logging.Info("Channel list has been set!");
-            Logging.Logging.Info("  > Channles: " + string.Join(", ", e.Channels));
+            Log.Info("Channel list has been set!");
+            Log.Info("  > Channles: " + string.Join(", ", e.Channels));
         }
 
         private void Monitor_OnServiceStarted(object sender, OnServiceStartedArgs e) {
-            Logging.Logging.Info("Service has started!");
+            Log.Info("Service has started!");
         }
 
         private void Monitor_OnStreamUpdate(object sender, OnStreamUpdateArgs e) {
             // What gets updated here?
 
-            Logging.Logging.Info("Update: " + e.Channel);
+            Log.Info("Update: " + e.Channel);
         }
 
+
+        /// <summary>
+        /// Called when stream went offlne<br/>
+        /// <c>Todo</c>: Usful Try {} catch {}
+        /// </summary>
+        /// <param name="sender">The sender object</param>
+        /// <param name="e">The event args</param>
         private void Monitor_OnStreamOffline(object sender, OnStreamOfflineArgs e) {
-            var configEventName = "OnStreamOffline";
-            Logging.Logging.Info("Offline: " + e.Channel);
-            Logging.Logging.Debug(e.Channel + " went offline! | Sender object: " + sender.ToString());
-            //Config.GetEventObjectsByTwitchChannelName(configEventName, e.Channel);
+            var configEventName = e.GetType().Name.Replace("args", "", StringComparison.OrdinalIgnoreCase);
+            Log.Info("Offline: " + e.Channel);
+            Log.Debug(e.Channel + " went offline!");
         }
 
-        private void Monitor_OnStreamOnline(object sender, OnStreamOnlineArgs e) {
-            var configEventName = "OnStreamOnline";
-            Logging.Logging.Info("Online: " + e.Channel);
-            Logging.Logging.Debug(e.Channel + " went online! | Sender object: " + sender.ToString());
-            var channelOffline = Config.GetEventObjectsByTwitchChannelName(configEventName, e.Channel);
 
-            if (channelOffline.Count > 0) {
-                foreach (var eventObject in channelOffline) {
-                    var webRequest = new WebRequest.WebRequest() {
-                        webHookUrl = (dynamic)channelOffline["WebHookUrl"],
-                        embed = (dynamic)channelOffline["Embed"]
+        /// <summary>
+        /// Called when stream went live<br/>
+        /// <c>Todo</c>: Usful Try {} catch {}
+        /// </summary>
+        /// <param name="sender">The sender object</param>
+        /// <param name="e">The event args</param>
+        private async void Monitor_OnStreamOnline(object sender, OnStreamOnlineArgs e) {
+            var configEventName = e.GetType().Name.Replace("args", "", StringComparison.OrdinalIgnoreCase);
+            Log.Info("Online: " + e.Channel);
+            Log.Debug(e.Channel + " went online!");
+            var channelOnline = Config.GetEventObjectsByTwitchChannelName(configEventName, e.Channel);
+
+            if (channelOnline.Count > 0) {
+                foreach (var eventObject in channelOnline) {
+                    var placeholderHelper = new PlaceholderHelper() {
+                        Stream = e.Stream,
+                        Channel = new Channel() {
+                            Name = e.Channel,
+                            User = await API.V5.Channels.GetChannelByIDAsync(e.Stream.UserId)
+                        }
                     };
+
+                    var embed = Parser.Deserialize(typeof(Embed), ((dynamic)eventObject.Value)["Embed"], placeholderHelper);
+                    new WebRequest() {
+                        webHookUrl = ((dynamic)eventObject.Value)["WebHookUrl"],
+                        embed = embed
+                    }.SendRequest();
                 }
-                //webRequest.
             }
         }
     }
