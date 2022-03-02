1db88f1b578cad936cd7c4569025e15b1278a0bc
xIRoXaSx <xIRoXaSx@LizardNetwork.net>
2022-03-02 23:19:33 +0100

    Modified default config

--- TwitchNotifier/src/config/DiscordEmbed.cs
+++ TwitchNotifier/src/config/DiscordEmbed.cs
@@ -4,7 +4,7 @@
     /// </summary>
     public class DiscordEmbed {
         public string Username { get; set; } = "%Channel.Name%";
-        public string AvatarUrl { get; set; } = "%Channel.User.Url%";
+        public string AvatarUrl { get; set; } = "%Channel.User.ProfileImageUrl%";
         public string Content { get; set; } = "Content above the embed (max 2048 characters)";
         public Embed Embed { get; set; } = new Embed();
     }
--- TwitchNotifier/src/config/EmbedFooter.cs
+++ TwitchNotifier/src/config/EmbedFooter.cs
@@ -4,6 +4,6 @@
     /// </summary>
     public class EmbedFooter {
         public string Text { get; set; } = "The footer text (max 2048 chars)";
-        public string IconUrl { get; set; } = "%Channel.User.Logo%";
+        public string IconUrl { get; set; } = "%Channel.User.ProfileImageUrl%";
     }
 }
